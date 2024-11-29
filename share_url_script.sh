d514aa90a1b5730b1ec83bc5f8ae0bea8b2403f4
joeldelpilar <joel.delpilar@eyevinn.se>
2024-11-29 09:13:19 +0100

    fix: updated script to return share url

--- share_url_script.sh
+++ share_url_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-read -p "Enter the bucket name (e.g., myBucket/myBucketFolder): " BUCKET
-
+read -p "Enter the bucket name (e.g., myBucket/myFolder): " BUCKET
 read -p "Enter the file name (e.g., example.mp4): " FILE_NAME
 
 if [[ -z "$BUCKET" || -z "$FILE_NAME" ]]; then
@@ -9,25 +8,17 @@ if [[ -z "$BUCKET" || -z "$FILE_NAME" ]]; then
   exit 1
 fi
 
-JSON_FILE="${FILE_NAME}_share.json"
+mc cp "$FILE_NAME" "$BUCKET/"
+if [ $? -ne 0 ]; then
+  echo "Failed to upload $FILE_NAME to $BUCKET."
+  exit 1
+fi
+echo "File $FILE_NAME uploaded to $BUCKET."
 
 SHARE_URL=$(mc share download "$BUCKET/$FILE_NAME" | grep -o "http[^ ]*" | tail -n 1)
-
 if [ -z "$SHARE_URL" ]; then
-  echo "Failed to generate presigned URL for $BUCKET/$FILE_NAME"
+  echo "Failed to generate presigned URL for $FILE_NAME in $BUCKET."
   exit 1
 fi
 
-echo "{ \"file\": \"$FILE_NAME\", \"url\": \"$SHARE_URL\" }" > "$JSON_FILE"
-echo "Presigned URL saved in $JSON_FILE"
-
-mc cp "$JSON_FILE" "$BUCKET/"
-
-if [ $? -eq 0 ]; then
-  echo "JSON file uploaded to $BUCKET/"
-else
-  echo "Failed to upload $JSON_FILE to $BUCKET/"
-  exit 1
-fi
-
-rm -f "$JSON_FILE"
+echo "Share URL: $SHARE_URL"
